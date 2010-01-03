bd7a91eca95771ebe57ed4cc3f2ec7a68d12115a
Vladimir Panteleev <vladimir@thecybershadow.net>
2010-01-03 17:12:58 +0200

    * separated Kwirk from algorithm

--- thread_boost.cpp
+++
@@ -1,7 +0,0 @@
-#include <boost/thread/thread.hpp>
-
-#define THREAD boost::thread*
-#define THREAD_CREATE(delegate) new boost::thread(delegate)
-#define THREAD_JOIN(thread) (thread)->join()
-#define THREAD_DESTROY(thread) delete (thread)
-

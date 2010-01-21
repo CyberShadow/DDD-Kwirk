62f5f6a816e3d25f1bf636a8ebc445b9e48e80c0
Deadc0de <davide.by.zero@gmail.com>
2010-01-20 22:51:09 -0800

    * made 26-validator.h compatible with the new code

--- Levels/26-validator.h
+++ Levels/26-validator.h
@@ -3,8 +3,8 @@ bool State::validate() const
 	for (int y=1; y<Y-1; y++)
 		for (int x=1; x<X-1; x++)
 		{
-			BYTE o = map[y][x] & OBJ_MASK;
-			if (o>0 && o <= OBJ_BLOCKMAX)
+			BYTE o = map[y][x] & CELL_MASK;
+			if (o>0 && o <= CELL_BLOCK)
 			{
 				for (char d=0;d<4;d++)
 				{

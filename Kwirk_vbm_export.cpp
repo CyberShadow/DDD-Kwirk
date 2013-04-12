c026b07d1c6fc61ac257e8eccee016f05a7dd5bc
Deadc0de <davide.by.zero@gmail.com>
2013-04-12 06:20:31 -0700

    Export to .VBM (VirtualBoyAdvance movie file) implemented

---
+++ Kwirk_vbm_export.cpp
@@ -0,0 +1,246 @@
+// To run this instead of a DDD search, add the following line to config.h:
+// #define PROBLEM_RELATED Kwirk_vbm_export
+// This must be run once for each level, from 0 to 29 consecutively.
+
+#define BIRDS_EYE_VIEW // against intuition, this actually takes less total time, even though it initially adds 2 extra frames; as an added bonus, it's nicer to watch
+
+int export_vbm()
+{
+	FILE *solution_in;
+	{} {            } solution_in = fopen(STRINGIZE(LEVEL)".txt",         "rt");
+	if (!solution_in) solution_in = fopen(STRINGIZE(LEVEL)"_vbm_mod.txt", "rt");
+	if (!solution_in) solution_in = fopen(STRINGIZE(LEVEL)"_vbm.txt",     "rt");
+	if (!solution_in)
+	{
+		fprintf(stderr, "Error reading solution file\n");
+		return -1;
+	}
+	FILE *vbm_out;
+	if (LEVEL==0)
+		vbm_out = fopen("Kwirk (UA) [optimized].vbm", "wb");
+	else
+		vbm_out = fopen("Kwirk (UA) [optimized].vbm", "ab");
+	if (!vbm_out)
+	{
+		fclose(solution_in);
+		fprintf(stderr, "Error writing vbm file\n");
+		return -1;
+	}
+
+	static WORD action_to_vbm[] = {
+	/* UP     */ 0x0040,
+	/* RIGHT  */ 0x0010,
+	/* DOWN   */ 0x0080,
+	/* LEFT   */ 0x0020,
+	/* SWITCH */ 0x0004,
+	};
+
+	WORD control;
+
+	if (LEVEL == 0)
+	{
+		static BYTE vbm_header[0x100] = {
+			0x56,0x42,0x4D,0x1A,0x01,0x00,0x00,0x00,0x62,0xEE,0xB2,0x45,0xC7,0xDD,0x00,0x00,
+			0x0F,0x03,0x00,0x00,0x00,0x01,0x00,0x30,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x00,
+			0x03,0x00,0x00,0x00,0x4B,0x57,0x49,0x52,0x4B,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+			0x00,0x0D,0x0A,0x6D,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x00,
+		};
+		strcpy((char*)(vbm_header+0x40), "CyberShadow and Deadcode");
+		fwrite(vbm_header, sizeof(vbm_header), 1, vbm_out);
+
+		control = 0x0000; // no buttons
+		for (int i=0; i<27; i++)
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+
+		control = 0x0008; // Start
+		for (int i=0; i<10; i++)
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+
+		control = 0x0000; // no buttons
+		for (int i=0; i<14; i++)
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+
+		// SELECT GAME -> GOING UP
+		control = 0x0001; // A
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+		control = 0x0000; // no buttons
+		for (int i=0; i<16; i++)
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+		// SELECT SKILL -> EASY
+		control = 0x0001; // A
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+		control = 0x0000; // no buttons
+		for (int i=0; i<24; i++)
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+		// SELECT FLOOR -> FL# 1
+		control = 0x0001; // A
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+		control = 0x0000; // no buttons
+		for (int i=0; i<24; i++)
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+#ifdef BIRDS_EYE_VIEW
+		// BIRD'S-EYE VIEW
+		control = 0x0080; // DOWN
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+		control = 0x0000; // no buttons
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+#endif
+		// SELECT DISPLAY -> DIAGONAL VIEW (or BIRD'S-EYE VIEW)
+		control = 0x0001; // A
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+		control = 0x0000; // no buttons
+		for (int i=0; i<24; i++)
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+	}
+
+	// GOING UP? -> START
+	control = 0x0001; // A
+	fwrite(&control, sizeof(WORD), 1, vbm_out);
+	control = 0x0000; // no buttons
+	for (int i=0; i<165; i++)
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+
+	if (LEVEL==15 || LEVEL==18)
+	{
+		control = 0x0000; // no buttons
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+	}
+	else
+	if (LEVEL==5 || LEVEL==10)
+	{
+		control = 0x0000; // no buttons
+		for (int i=0; i<2; i++)
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+	}
+	else
+#ifdef BIRDS_EYE_VIEW
+	if (LEVEL==1 || LEVEL==6 || LEVEL==8 || LEVEL==9 || LEVEL==22 || LEVEL==27)
+	{
+		control = 0x0000; // no buttons
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+	}
+#else
+	if (LEVEL==14 || LEVEL==25 || LEVEL==26 || LEVEL==28)
+	{
+		control = 0x0000; // no buttons
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+	}
+#endif
+
+	int frames = 0;
+	int steps = -1;
+	int switches = 0;
+	try
+	{
+        State state = State::initial;
+		while (state.playersLeft())
+		{
+			Action action;
+
+			char input[1024];
+			fgets(input,sizeof(input),solution_in);
+			size_t len=strlen(input);
+			if (input[len-1]!='\n') error("Bad input!"); else input[--len]='\0';
+			if (input[len-1]=='!' || input[len-1]=='~')
+				input[--len]='\0';
+			for (action=ACTION_FIRST; action<=NONE; action++)
+				if (strcmp(input, actionNames[action])==0)
+					goto valid_action;
+			error(format("Unrecognized action '%s'", input));
+		valid_action:
+			steps++;
+			if (steps==0 && action!=NONE)
+				error("Expected 'None' action");
+			if (steps>0 && action==NONE)
+				error("Did not expect 'None' action");
+			for (int i=0; i<Y; i++)
+			{
+				fgets(input,sizeof(input),solution_in);
+				size_t len=strlen(input);
+				if (input[len-1]!='\n') error("Bad input!"); else input[--len]='\0';
+				if (input[0] != '#')
+					error(format("Unexpected input: '%s'", input));
+			}
+			if (action==NONE)
+				continue;
+			if (action==SWITCH)
+				switches++;
+
+			int res = state.perform<true,false>(action);
+			if (res <= 0)
+				error("Bad action!");
+
+#if 1
+			if (action==SWITCH)
+			{
+				fwrite(&action_to_vbm[action], sizeof(WORD), 1, vbm_out);
+				for (int i=0; i<res-1; i++)
+					fwrite(&control, sizeof(WORD), 1, vbm_out);
+			}
+			else
+			{
+				control = 0x0000;
+				fwrite(&control, sizeof(WORD), 1, vbm_out);
+				fwrite(&action_to_vbm[action], sizeof(WORD), 1, vbm_out);
+				control = 0x0000;
+				for (int i=1; i<res-1; i++)
+					fwrite(&control, sizeof(WORD), 1, vbm_out);
+			}
+#else
+			for (int i=0; i<res; i++)
+				fwrite(&action_to_vbm[action], sizeof(WORD), 1, vbm_out);
+#endif
+
+			frames += res;
+		}
+		printf("%s", state.toString());
+	}
+	catch (const char* s)
+	{
+		puts(s);
+	}
+	printf("Total %d+%d steps, %d frames (%1.3f seconds)\n", steps-switches, switches, frames, frames/60.);
+
+	control = 0x0000; // no buttons
+	for (int i=0; i<182; i++)
+		fwrite(&control, sizeof(WORD), 1, vbm_out);
+
+	control = 0x0008; // Start
+	fwrite(&control, sizeof(WORD), 1, vbm_out);
+
+	if (LEVEL != 29)
+	{
+		if (LEVEL % 10 == 9)
+		{
+			control = 0x0000; // no buttons
+			for (int i=0; i<254; i++)
+				fwrite(&control, sizeof(WORD), 1, vbm_out);
+
+			control = 0x0008; // Start
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+
+			control = 0x0000; // no buttons
+			for (int i=0; i<31; i++)
+				fwrite(&control, sizeof(WORD), 1, vbm_out);
+		}
+		else
+		{
+			control = 0x0000; // no buttons
+			for (int i=0; i<45; i++)
+				fwrite(&control, sizeof(WORD), 1, vbm_out);
+#ifndef BIRDS_EYE_VIEW
+			fwrite(&control, sizeof(WORD), 1, vbm_out);
+#endif
+		}
+	}
+
+	fclose(solution_in);
+	fclose(vbm_out);
+	return 0;
+}
+
+int run_related(int argc, const char* argv[])
+{
+	return export_vbm();
+	return 0;
+}

71f69c54aa3fd6d04d083c4224936530c752c68c
David Ellsworth <david@worms2d.info>
2024-02-21 01:14:33 -0800

    BK2 Export: Add BizHawk 2.3.2 mode

--- Kwirk_bk2_export.cpp
+++ Kwirk_bk2_export.cpp
@@ -4,7 +4,28 @@
 
 #define BIRDS_EYE_VIEW // against intuition, this actually takes less total time, even though it initially adds 1 extra frame; as an added bonus, it's nicer to watch
 
-#define BIZHAWK_2_9_1 // without this, compatibilitiy is with BizHawk 2.5.2
+//#define BIZHAWK_2_3_2 // requires Config -> Cores -> GB -> GBHawk
+//#define BIZHAWK_2_5_2
+#define BIZHAWK_2_9_1
+
+#if defined(BIZHAWK_2_3_2)
+	#if defined(BIZHAWK_2_5_2) || defined(BIZHAWK_2_9_1)
+		#error Must define exactly one
+	#endif
+	#define BIZHAWK_VERSION "2.3.2"
+#elif defined(BIZHAWK_2_5_2)
+	#if defined(BIZHAWK_2_3_2) || defined(BIZHAWK_2_9_1)
+		#error Must define exactly one
+	#endif
+	#define BIZHAWK_VERSION "2.5.2"
+#elif defined(BIZHAWK_2_9_1)
+	#if defined(BIZHAWK_2_3_2) || defined(BIZHAWK_2_5_2)
+		#error Must define exactly one
+	#endif
+	#define BIZHAWK_VERSION "2.9.1"
+#else
+	#error Must define exactly one
+#endif
 
 int export_bk2()
 {
@@ -20,9 +41,9 @@ int export_bk2()
 	}
 	FILE *bk2_out;
 	if (LEVEL==0)
-		bk2_out = fopen("Kwirk (UA) [optimized] 2.9.1/Input Log.txt", "wt");
+		bk2_out = fopen("Kwirk (UA) [optimized] " BIZHAWK_VERSION "/Input Log.txt", "wt");
 	else
-		bk2_out = fopen("Kwirk (UA) [optimized] 2.9.1/Input Log.txt", "at");
+		bk2_out = fopen("Kwirk (UA) [optimized] " BIZHAWK_VERSION "/Input Log.txt", "at");
 	if (!bk2_out)
 	{
 		fclose(solution_in);
@@ -47,17 +68,29 @@ int export_bk2()
 			"[Input]\n"
 			"LogKey:#P1 Up|P1 Down|P1 Left|P1 Right|P1 Start|P1 Select|P1 B|P1 A|P1 Power|\n", bk2_out);
 
+#ifdef BIZHAWK_2_3_2
+		for (int i=0; i<366; i++)
+#else
 		for (int i=0; i<378; i++)
+#endif
 			fputs(action_to_bk2[NONE], bk2_out);
 
 		fputs(pressStart, bk2_out);
 
+#ifdef BIZHAWK_2_3_2
+		for (int i=0; i<14; i++)
+#else
 		for (int i=0; i<18; i++)
+#endif
 			fputs(action_to_bk2[NONE], bk2_out);
 
 		// SELECT GAME -> GOING UP
 		fputs(pressA, bk2_out);
+#ifdef BIZHAWK_2_3_2
+		for (int i=0; i<15; i++)
+#else
 		for (int i=0; i<16; i++)
+#endif
 			fputs(action_to_bk2[NONE], bk2_out);
 		// SELECT SKILL -> EASY
 		fputs(pressStart, bk2_out);
@@ -69,23 +102,46 @@ int export_bk2()
 			fputs(action_to_bk2[NONE], bk2_out);
 		// SELECT FLOOR -> FL# 1
 		fputs(pressA, bk2_out);
+#ifdef BIZHAWK_2_3_2
+		for (int i=0; i<22; i++)
+#else
 		for (int i=0; i<24; i++)
+#endif
 			fputs(action_to_bk2[NONE], bk2_out);
 #ifdef BIRDS_EYE_VIEW
+	#ifdef BIZHAWK_2_3_2
+		fputs(action_to_bk2[NONE], bk2_out);
+	#endif
 		// BIRD'S-EYE VIEW
 		fputs(action_to_bk2[DOWN], bk2_out);
 #endif
 		fputs(action_to_bk2[NONE], bk2_out);
 		// SELECT DISPLAY -> DIAGONAL VIEW (or BIRD'S-EYE VIEW)
+#ifdef BIZHAWK_2_3_2
+		fputs(pressStart, bk2_out);
+#else
 		fputs(pressA, bk2_out);
+#endif
+#ifdef BIZHAWK_2_3_2
+		for (int i=0; i<23; i++)
+#else
 		for (int i=0; i<27; i++)
+#endif
 			fputs(action_to_bk2[NONE], bk2_out);
 	}
 
 	// GOING UP? -> START
+#ifdef BIZHAWK_2_3_2
+	fputs(pressA, bk2_out);
+#else
 	fputs(pressStart, bk2_out);
+#endif
 	{
+#ifdef BIZHAWK_2_3_2
+		int delay = 164;
+#else
 		int delay = 167;
+#endif
 		if (LEVEL != 0)
 			delay += 1;
 		if (LEVEL==3 || LEVEL==15)
@@ -108,6 +164,13 @@ int export_bk2()
 			delay += 1;
 		if (LEVEL==14 || LEVEL==25 || LEVEL==26 || LEVEL==28)
 			delay += 1;
+#endif
+#ifdef BIZHAWK_2_3_2
+		if (LEVEL==3)
+			delay += 1;
+		else
+		if (LEVEL==20)
+			delay += 2;
 #endif
 #ifdef BIZHAWK_2_9_1
 		if (LEVEL==3)
@@ -205,14 +268,22 @@ int export_bk2()
 		fputs("[/Input]\n", bk2_out);
 	else
 	{
+#ifdef BIZHAWK_2_3_2
+		for (int i=0; i<182; i++)
+#else
 		for (int i=0; i<191; i++)
+#endif
 			fputs(action_to_bk2[NONE], bk2_out);
 
 		fputs(pressStart, bk2_out);
 
 		if (LEVEL % 10 == 9)
 		{
+#ifdef BIZHAWK_2_3_2
+			for (int i=0; i<253; i++)
+#else
 			for (int i=0; i<254; i++)
+#endif
 				fputs(action_to_bk2[NONE], bk2_out);
 #ifndef BIRDS_EYE_VIEW
 			fputs(action_to_bk2[NONE], bk2_out);
@@ -220,12 +291,20 @@ int export_bk2()
 
 			fputs(pressStart, bk2_out);
 
+#ifdef BIZHAWK_2_3_2
+			for (int i=0; i<31; i++)
+#else
 			for (int i=0; i<33; i++)
+#endif
 				fputs(action_to_bk2[NONE], bk2_out);
 		}
 		else
 		{
+#ifdef BIZHAWK_2_3_2
+			for (int i=0; i<45; i++)
+#else
 			for (int i=0; i<57; i++)
+#endif
 				fputs(action_to_bk2[NONE], bk2_out);
 #ifndef BIRDS_EYE_VIEW
 			fputs(action_to_bk2[NONE], bk2_out);

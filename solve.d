9d320cabd7793388e248a2b0faf5a5e6753f2396
Vladimir Panteleev <git@cy.md>
2024-02-23 15:45:05 +0000

    solve: Print more stats while solving

--- solve.d
+++ solve.d
@@ -28,26 +28,33 @@ void program(
 	{
 		assert(frameNumber < statesAtFrame.length, "No more states.");
 		auto set = statesAtFrame[frameNumber];
-		stderr.writefln("Frame %d: %d / %d",
+
+		stderr.writefln("Frame %d: %d states, %d nodes",
 			frameNumber,
 			set.count,
 			set.uniqueNodes,
 		);
 
 		set = set.subtract(seenStates);
+		stderr.writefln("  Deduplicated: %d states, %d nodes", set.count, set.uniqueNodes);
+
 		seenStates = seenStates.merge(set);
+		stderr.writefln("  Total: %d states, %d nodes", seenStates.count, seenStates.uniqueNodes);
 		seenStates = seenStates.optimize();
+		stderr.writefln("    Optimized: %d nodes", seenStates.uniqueNodes);
 
 		set = set.optimize();
-		stderr.writefln("Optimized: %d", set.uniqueNodes);
+		stderr.writefln("  Optimized: %d nodes", set.uniqueNodes);
 
 		foreach (action; Action.init .. enumLength!Action)
 		{
 			Vars v;
 			v.visitor = Visitor(set);
 
+			ulong numIterations;
 			while (v.next())
 			{
+				numIterations++;
 				auto duration = perform(level, v, action);
 				if (duration < 0)
 					continue;
@@ -56,6 +63,7 @@ void program(
 					statesAtFrame.length = nextFrame + 1;
 				statesAtFrame[nextFrame] = statesAtFrame[nextFrame].merge(v.visitor.currentSubset);
 			}
+			stderr.writefln("  Processed %s in %d iterations.", action, numIterations);
 		}
 	}
 }
